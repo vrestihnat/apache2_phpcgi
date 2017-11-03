f129a514452516808f2e3308478e131e17e69408
vrestihnat <bfj@bflm.eu>
2017-11-04 00:55:34 +0100

    add mysql

---
+++ build_base_dir_w10.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /
+ln -s /mnt/c
+mkdir -p /c/docker/sources/unix
+mkdir -p /c/docker/sources/vs
+mkdir -p /c/docker/apache/cfg
+mkdir -p /c/docker/logs/apache
+mkdir -p /c/docker/php/cfg
+mkdir -p /c/docker/php/php5.2.17
+mkdir -p /c/docker/php/php5.3.29
+mkdir -p /c/docker/php/php5.4.45
+mkdir -p /c/docker/php/php5.6.29
+mkdir -p /c/docker/repos
+cd ~
+ln -s /c/docker
+
+
+
